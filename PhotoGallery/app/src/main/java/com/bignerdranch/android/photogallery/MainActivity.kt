a013bf9d209317b888e78a307477cb3fc7f9c54f
Salma KHD <khodaeisalma8@gmail.com>
2023-02-05 11:25:34 -0800

    Delete all the code retrieved from external sources

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/MainActivity.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/MainActivity.kt
@@ -1,21 +1,3 @@
-package com.bignerdranch.android.photogallery
-
-import android.content.Context
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    companion object {
-        // this function will be used to launch the app programmatically
-        fun newIntent(context: Context): Intent {
-            return Intent(context, MainActivity::class.java)
-        }
-    }
-
-}
+/*
+	Code deleted for copyright reasons.
+*/
--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryApplication.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryApplication.kt
@@ -1,36 +1,3 @@
-package com.bignerdranch.android.photogallery
-
-import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
-
-const val NOTIFICATION_CHANNEL_ID = "flickr_poll"
-// this class extends the Application class which ensures all the objects
-// created inside it will persist till the app process is killed.
-class PhotoGalleryApplication: Application() {
-    override fun onCreate() {
-        super.onCreate()
-        PreferencesRepository.initialize(this)
-
-        // create a channel for notifications: API >= 26 needed
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = getString(R.string.notification_channel_name)
-
-            // assign priority to the channel
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-
-            // create a channel for showing a min of one category of notifications
-            val channel =
-                NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance)
-
-            // get NotificationManager object for creating a channel
-            val notificationManager: NotificationManager =
-                getSystemService(NotificationManager::class.java)
-
-            // create channel
-            notificationManager.createNotificationChannel(channel)
-
-        }
-    }
-}
+/*
+	Code deleted for copyright reasons.
+*/
