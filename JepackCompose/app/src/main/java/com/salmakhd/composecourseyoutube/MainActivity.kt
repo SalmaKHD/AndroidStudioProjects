f396b9f944dc9b07a060b4ef16c2f47898034b37
Salma KHD <khodaeisalma8@gmail.com>
2023-02-14 10:04:16 -0800

    Add code for Jetpack Compose experimnet3

--- JepackCompose/app/src/main/java/com/salmakhd/composecourseyoutube/MainActivity.kt
+++ JepackCompose/app/src/main/java/com/salmakhd/composecourseyoutube/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
@@ -18,62 +22,55 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 import com.salmakhd.composecourseyoutube.ui.theme.ComposeCourseYouTubeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // define a FontFamily object
-        val fontFamily = FontFamily(
-            Font(R.font.gentium_book_plus_regular, FontWeight.Normal),
-            Font(R.font.gentium_book_plus_bold, FontWeight.Bold),
-        )
-
-        setContent {
-            ComposeCourseYouTubeTheme {
-                Box(
+        setContent() {
+            Column(Modifier.fillMaxSize()) {
+                // remember will make the variable be declared only once (not affected by recomposition)
+                val color = remember { mutableStateOf(Color.Yellow) }
+                ColorBox(
                     modifier = Modifier
+                        .weight(1f)
                         .fillMaxSize()
-                        .background(
-                            color = Color(0xFF101010)
-                        )
                 ) {
-                    Text(
-                        // apply different styles to separate segments of the text
-                        text = buildAnnotatedString {
-
-                            // this style will be applied to the letter "J"
-                            withStyle(
-                                style = SpanStyle(
-                                    color = Color.Green,
-                                    fontSize = 50.sp
-                                )
-                            ) {
-                                append("J")
-                            }
-
-                            append("etpack ")
-
-                            withStyle(
-                                style = SpanStyle(
-                                    color = Color.Green,
-                                    fontSize = 50.sp
-                                )
-                            ) {
-                                append("C")
-                            }
-
-                            append("ompose")
-                        },
-                        // default style
-                        color = Color.White,
-                        fontSize = 30.sp,
-                        fontFamily = fontFamily,
-                        fontWeight = FontWeight.Bold,
-                        textDecoration = TextDecoration.Underline
-                    )
+                    /*
+                    Explanation of the logic:
+                    since the clickable() function is inside the composable,
+                    we have to pass this function which will change the color value
+                    from within the composable.
+                     */
+                    color.value = it
                 }
+                Box(modifier = Modifier
+                    .background(color.value)
+                    .weight(1f)
+                    .fillMaxSize()
+                )
             }
         }
     }
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .background(Color.Red)
+            .clickable {
+                updateColor(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f
+                    )
+                )
+            }
+    )
 }
---
+++ JepackCompose/composables/Experimnet3/README.md
@@ -0,0 +1,4 @@
+### Final Look
+![experiment2-screenshot](https://github.com/SalmaKHD/AndroidStudioProjects/blob/main/JepackCompose/composables/Experiment2/experiment2-screenshot.PNG?raw=true)
+## Credit
+- https://youtu.be/nm_LNJWHi9A
