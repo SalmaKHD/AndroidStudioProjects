53cc0f716de2f6842bb09c5553dd111ac7f5cabd
SalmaKHD <khodaeisalma8@gmail.com>
2023-02-25 13:34:18 -0800

    Add code for Affirmations project

--- Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -14,11 +14,28 @@
  * limitations under the License.
  */
 package com.example.affirmations
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,5 +50,66 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AffirmationApp() {
     AffirmationsTheme {
+        AffirmationList(
+            affirmationList = Datasource().loadAffirmations())
+    }
+}
+
+@Composable
+fun AffirmationList(
+    affirmationList: List<Affirmation>,
+    modifier: Modifier = Modifier
+) {
+    // define a LazyColumn for displaying a list of cards
+    LazyColumn {
+        // call the items() method for adding items to the list
+        items(affirmationList.size) { affirmation ->
+            AffirmationCard(affirmation = affirmationList[affirmation])
+        }
+    }
+}
+
+// Preview composable functions should be markes as private
+@Composable
+@Preview
+private fun AffirmationCardPreview() {
+    AffirmationCard(
+        affirmation = Affirmation(
+            R.string.affirmation1,
+            R.drawable.image1
+        )
+    )
+}
+
+@Composable
+fun AffirmationCard(
+    affirmation: Affirmation,
+    modifier: Modifier = Modifier
+) {
+    // modifier argument should come after all required arguments
+    Card (
+        modifier = modifier
+            .padding(8.dp),
+        elevation = 4.dp
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                // determines how the required dimensions will affect
+                // the image
+                contentScale = ContentScale.Crop
+            )
+
+            Text(
+                text = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier
+                    .padding(16.dp),
+                style = MaterialTheme.typography.h6
+            )
+        }
     }
 }
--- Affirmations/app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -15,12 +15,15 @@
  */
 package com.example.affirmations.data
 import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
 
 /**
  * [Datasource] generates a list of [Affirmation]
  */
 class Datasource() {
-    /*fun loadAffirmations(): List<Affirmation> {
+    // this function returns a list of integer pairs representing string
+    // and image resources stored in project directories
+    fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation>(
             Affirmation(R.string.affirmation1, R.drawable.image1),
             Affirmation(R.string.affirmation2, R.drawable.image2),
@@ -32,5 +35,6 @@ class Datasource() {
             Affirmation(R.string.affirmation8, R.drawable.image8),
             Affirmation(R.string.affirmation9, R.drawable.image9),
             Affirmation(R.string.affirmation10, R.drawable.image10))
-    }*/
+    }
 }
+
